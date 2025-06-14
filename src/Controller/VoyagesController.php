4b96706f484c16db22dc7a0a620ef11112f1a16f
TOGNISSE <TOGNISSE@DESKTOP-44AA7O3>
2025-06-14 22:07:58 +0200

    phase 10: ORM (affichage détaillée d'une visite)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -50,4 +50,11 @@ class VoyagesController extends AbstractController{
             'visites' => $visites
         ]);
     }
+    #[Route('/voyages/voyage/{id}', name: 'voyages.showone')]
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render('pages/voyage.html.twig', [
+            'visite' => $visite
+        ]);
+    }
 }
