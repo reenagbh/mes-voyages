c3fd439f1f1dbeaa8cd8a9a48c4fc844d8d27a20
TOGNISSE <TOGNISSE@DESKTOP-44AA7O3>
2025-06-17 16:20:20 +0200

    phase 16: ORM (GESTION DU LIEN ENTRE LES TABLES // PARTIE ADMIN)

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -3,12 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Visite;
+use App\Entity\Environnement;
 use DateTime;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class VisiteType extends AbstractType
 {
@@ -36,6 +38,13 @@ class VisiteType extends AbstractType
             ->add('tempmax', null, [
                 'label' => 't° max'
             ])
+            ->add('environnements', EntityType::class, [
+                'class' => Environnement::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+                'required' => false,
+            ])
+
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer'
             ])
